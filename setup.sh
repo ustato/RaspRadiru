1453af5729fc401e2558f6bbe8d0e313c0dd5e28
Atsuto Inage <atsuto.fps@gmail.com>
2018-05-28 19:27:28 +0900

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,2 @@
+alias radio_r1='omxplayer --timeout 60s -o local https://nhkradiobkr1-i.akamaihd.net/hls/live/512291/1-r1/1-r1-01.m3u8'
+alias radio_r2='omxplayer --timeout 60s -o local https://nhkradioakr2-i.akamaihd.net/hls/live/511929/1-r2/1-r2-01.m3u8'
